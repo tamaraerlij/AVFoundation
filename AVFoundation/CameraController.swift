e442143a826481fa120d494763e6e4ea3499bf05
Tamara <tamara.erlij@gmail.com>
2020-04-29 15:15:39 -0300

    Reunindo os 2 tutoriais em 1 projeto

---
+++ AVFoundation/CameraController.swift
@@ -0,0 +1,42 @@
+//
+//  CameraController.swift
+//  AVFoundation
+//
+//  Created by Tamara Erlij on 29/04/20.
+//  Copyright © 2020 Tamara Erlij. All rights reserved.
+//
+
+import Foundation
+import AVFoundation
+
+class CameraController { }
+
+//func prepare(completionHandler: @escaping (Error?) -> Void) {
+//    func createCaptureSession() { }
+//      func configureCaptureDevices() throws { }
+//      func configureDeviceInputs() throws { }
+//      func configurePhotoOutput() throws { }
+//      
+//      DispatchQueue(label: "prepare").async {
+//          do {
+//              createCaptureSession()
+//              try configureCaptureDevices()
+//              try configureDeviceInputs()
+//              try configurePhotoOutput()
+//          }
+//              
+//          catch {
+//              DispatchQueue.main.async {
+//                  completionHandler(error)
+//              }
+//              
+//              return
+//          }
+//          
+//          DispatchQueue.main.async {
+//              completionHandler(nil)
+//          }
+//      }
+//    
+    
+//}
---
+++ AVFoundation/ViewController2.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController2.swift
+//  AVFoundation
+//
+//  Created by Tamara Erlij on 29/04/20.
+//  Copyright © 2020 Tamara Erlij. All rights reserved.
+//
+
+import UIKit
+
+class ViewController2: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
